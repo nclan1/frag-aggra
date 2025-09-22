c4c03fc3ff317a055863c3f34b554237a71586c7
nolan <144292887+nclan1@users.noreply.github.com>
2025-09-21 20:52:31 -0400

    feat: added first src

---
+++ src/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Example listing text
+	sample_listing := "Just looking to sell, No trades. Shipping included in price. All with boxes.\n\nTester D&G light blue edt 125ml 90% 33$\n\nMaison Asrar DXP 95/100ml 30$\n\nBundled: 55 shipped\n\nIf your looking to add on I have 2.5ml decants of\n\nLayton $6\n\nBIR Extradose $5\n\nDior homme sport $5\n\nMyself edp $5\n\nPayPal G&S only\n\nhttps://imgur.com/a/cpoVx0j\n\nDecants:\n\nhttps://imgur.com/a/J6aEMmb"
+
+	fmt.Println("hello world")
+	fmt.Println(sample_listing)
+}
